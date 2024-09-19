c308c0670ee70be95afb00f7419422e344d43abe
Md. Rabiul Alam <mavenrabiul@gmail.com>
2024-09-19 23:09:58 +0600

    question no.4 solved
    question no.4 solved

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-/* 1. Write a function reverseString(str) that takes a string and returns it reversed. Ex: reverseString("hello"); // Output: "olleh"*/
+/* 1. Write a function reverseString(str) that takes a string and returns it reversed. Ex: reverseString("hello"); // Output: "olleh"  */
+
 
 function reverseString(str) {
     return str.split("").reverse().join("");
 }
 console.log(reverseString("hello"));
 
-/*2. Write a function sumArray(arr) that takes an array of numbers and returns the sum of all the numbers. Ex: sumArray([1, 2, 3, 4]); // Output: 10*/
+
+
+/*2. Write a function sumArray(arr) that takes an array of numbers and returns the sum of all the numbers. Ex: sumArray([1, 2, 3, 4]); // Output:10   */
+
+
 
 function sumArray(arr) {
     let sum = 0;
@@ -18,7 +23,10 @@ function sumArray(arr) {
 }
 console.log(sumArray([1, 2, 3, 4]));
 
-/* 3. Write a function removeDuplicates(arr) that takes an array and returns a new array with only unique values (duplicates removed). Ex: removeDuplicates([5, 5, 5, 6, 7]); // Output: [5, 6, 7]*/
+
+
+/* 3. Write a function removeDuplicates(arr) that takes an array and returns a new array with only unique values (duplicates removed). Ex: removeDuplicates([5, 5, 5, 6, 7]); // Output: [5, 6, 7]  */
+
 
 function removeDuplicates(arr) {
     let seen = {};
@@ -36,3 +44,21 @@ function removeDuplicates(arr) {
 }
 
 console.log(removeDuplicates([5, 5, 5, 6, 7]));
+
+
+
+/* 4.  Write a function celsiusToFahrenheit(celsius) that takes a temperature in Celsius and converts it to Fahrenheit. The formula to convert Celsius to Fahrenheit is:
+
+Fahrenheit=(celsius * 9/5)+32
+
+Example: console.log(celsiusToFahrenheit(25)); // Output: 77   */
+
+
+
+function celsiusToFahrenheit(celsius) {
+
+    let fahrenheit = (celsius * 9 / 5) + 32;  // formula to convert Celsius to Fahrenheit
+    return fahrenheit;
+}
+
+console.log(celsiusToFahrenheit(25));
